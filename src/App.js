eed7d65143d762af754b811ae2c4aae0379e0a89
James Smith <jwsmith2340@yahoo.com>
2022-02-06 21:02:29 -0500

    categories bar styled, non-functional, but very pretty

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import Header from './components/Header'
+import Header from './components/Header/Header'
+import Navbar from './components/Navbar/Navbar'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import './Navbar.scss'
+
+function Navbar() {
+    return (
+        <div className="navbar-box">
+            <div className="categories">
+                <h5>Guitars</h5>
+            </div>
+            <div className="login">
+                <h5>Login/Signup</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
